323b55c1e67d4bcf90d224878a333fbdeead2eb5
Suraj <surajkhosla79@gmail.com>
2021-02-27 17:57:30 -0600

    Added ebay_api_testing, edited main.py, added ebaything.html

---
+++ ebay_api_testing.py
@@ -0,0 +1,23 @@
+from ebaysdk.finding import Connection as Finding
+from ebaysdk.exception import ConnectionError
+
+try:
+    api = Finding(appid="SurajKho-hackutd-PRD-cdc74433e-5e966209", config_file=None)
+    response = api.execute('findItemsAdvanced', {'keywords': 'toilet paper'})
+    c = (response.dict()['searchResult']['item'])
+    result_array = []
+    result_cost = []
+    result_images = []
+    for item in range(10):
+        result_array.append(c[item]['title'])
+        result_images.append(c[item]['galleryURL'])
+        result_cost.append(c[item]['sellingStatus']['currentPrice']['value'])
+    print(result_array)
+    print(result_cost)
+    print(result_images)
+except ConnectionError as e:
+    print(e)
+    print(e.response.dict())
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, url_for, request, redirect
+from ebaysdk.finding import Connection as Finding
+from ebaysdk.exception import ConnectionError
+from ebay_api_testing import result_array
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def display_index():
     return render_template('index.html')
+
+
+@app.route('/ebaything')
+def display_ebaything():
+    c = result_array
+    return render_template('ebaything.html', array=c)
