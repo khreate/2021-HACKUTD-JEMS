dc91dc253c36e8191b5df762421b9e08a8a990ae
Suraj <surajkhosla79@gmail.com>
2021-02-27 21:54:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ from ebay_api_testing import *
 
 app = Flask(__name__)
 
+keyword = ' '
+
 
 @app.route('/')
 def display_index():
     return render_template('index.html')
 
 
-@app.route('/ebaysearch')
-def display_ebaySearch():
-    return render_template('ebaySearch.html')
+@app.route('/ebaysearch', methods=["POST", "GET"])
+def get_search_query():
+    keyword = request.data.decode('utf-8')
+    return keyword
 
 
 @app.route('/ebaything', methods=["POST", "GET"])
 def display_ebaything():
-    keyword = request.data.decode('utf-8')
+    get_search_query()
     print(request.data)
     c = ebay_title(keyword)
     cost = ebay_cost(keyword)
